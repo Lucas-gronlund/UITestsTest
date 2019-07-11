ed7aaf456b7871d053123f09866fb7b6ab0343dd
lucas-gronlund <lucas.gron@gmail.com>
2019-07-11 11:28:20 +0200

    Initial Commit

---
+++ UITestsProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITestsProject
+//
+//  Created by Lucas Grönlund on 2019-07-11.
+//  Copyright © 2019 Code and Release. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
